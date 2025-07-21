6fa64d9dff6ea08a467e6aec6b4e5f3bee1ae1d8
Henrique Silveira Marques <henrique.silveira@estudante.ufjf.br>
2025-07-21 19:20:11 -0300

    Instruções do slide

---
+++ main.js
@@ -0,0 +1,25 @@
+import { getLista, limpaLista } from "./lista.js"
+
+const olItens = document.querySelector("#itens");
+const pEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
+
+btnLimpar.addEventListener('click', limparItensDeLista);
+
+atualizarLista();
+
+function atualizarLista() {
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for (let i = 0; i < lista.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
+
+function limparItensDeLista() {
+    limpaLista();
+    atualizarLista();
+}
